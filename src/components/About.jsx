eb5966fbcfb83d25d6995917e5e336ce22ddfc6d
RaveBlue <my-raveblue21@yahoo.com>
2026-02-03 00:19:30 -0700

    updated pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,78 @@
 import React from "react";
 import "./About.css";
 import cloud from "./cloud.png";
+import Ravenimg from "./Ravenimg.png";
+import bluecode from "./bluecode.png";
+import programming from "./programming.png";
+//import programmer from "./programmer.png";
+import programmer2 from "./programmer2.png";
 
 const About = () => {
   return (
-    <div className="top-container">
-      <img
-        className="top-cloud"
-        src={cloud}
-        alt="icon"
-        style={{ width: "120px", marginBottom: "20px" }}
-      />
-      <h1 className="cursive-name has-text-weight-medium">About </h1>
-      <div className="dev-text"></div>
+    <div className="main">
+      <div className="top-container">
+        <img
+          className="top-cloud"
+          src={cloud}
+          alt="icon"
+          style={{ width: "120px", marginBottom: "20px" }}
+        />
+        <h1 className="cursive-name has-text-weight-medium">About </h1>
+        <div className="dev-text"></div>
 
-      <img
-        className="bottom-cloud-right"
-        src={cloud}
-        alt="cloud"
-        style={{
-          width: "100px",
-          position: "absolute",
-          bottom: "0",
-          right: "40px",
-        }}
-      />
+        <img
+          className="bottom-cloud-right"
+          src={cloud}
+          alt="cloud"
+          style={{
+            width: "100px",
+            position: "absolute",
+            bottom: "0",
+            right: "40px",
+          }}
+        />
+      </div>
+      <div className="middle-container">
+        <div className="profile">
+          <img className="me" src={programmer2} alt="person" />
+          <h2>Hello.</h2>
+          <p className="intro">
+            My name is Raven. I enjoy creating web applications. I 💗 matcha
+            lattes and boba tea.
+          </p>
+        </div>
+        <hr />
+        <div className="skills">
+          <h2>My Skills</h2>
+          <div className="skill-row">
+            <img className="code" src={bluecode} alt="code" />
+            <h3>Design & Development</h3>
+            <p>
+              I first decided to learn to code when I came across this
+              multimedia class, and we learned about different media careers. I
+              came across web development and found it very interesting, so I
+              switched from a degree in Physical therapy to a degree in web
+              design and development. I then decided to join a coding bootcamp
+              and I was able to join Fullstack Academy through Colorado State
+              University.
+            </p>
+          </div>
+          <div className="skill-row">
+            <img className="more" src={programming} alt="skills" />
+            <h3>Coding skills and Education</h3>
+            <p>
+              Web design and development graduate with additional training from
+              Fullstack Academy software engineering immersive bootcamp. I have
+              skills using HTML, CSS, JavaScript, React, Node.js, Express.js,
+              PostgreSQL, Git and Python. I also enjoy learning other languages
+              such as French and Korean. I like photography, and I like to
+              crochet as well as health and fitness.
+            </p>
+          </div>
+        </div>
+
+        <img className="raven" src={Ravenimg} alt="person" />
+      </div>
     </div>
   );
 };
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,80 @@
 import React from "react";
 import "./Contact.css";
 import cloud from "./cloud.png";
+import clo from "./clo.png";
+import contactus from "./contactus.png";
+import application from "./application.png";
 
 const Contact = () => {
   return (
-    <div className="top-container">
-      <img
-        className="top-cloud"
-        src={cloud}
-        alt="icon"
-        style={{ width: "120px", marginBottom: "20px" }}
-      />
-      <h1 className="cursive-name has-text-weight-medium">Contact Me</h1>
-      <div className="dev-text"></div>
+    <div className="main">
+      <div className="top-container">
+        <img
+          className="top-cloud"
+          src={cloud}
+          alt="icon"
+          style={{ width: "120px", marginBottom: "20px" }}
+        />
+        <h1 className="cursive-name has-text-weight-medium">Contact Me</h1>
+        <div className="dev-text"></div>
 
-      <img
-        className="bottom-cloud-right"
-        src={cloud}
-        alt="cloud"
-        style={{
-          width: "100px",
-          position: "absolute",
-          bottom: "0",
-          right: "40px",
-        }}
-      />
+        <img
+          className="bottom-cloud-right"
+          src={cloud}
+          alt="cloud"
+          style={{
+            width: "100px",
+            position: "absolute",
+            bottom: "0",
+            right: "40px",
+          }}
+        />
+      </div>
+
+      <div className="middle-container">
+        <div className="profile">
+          <img className="me" src={clo} alt="contact" />
+          <h2>Hello.</h2>
+          <p className="intro">
+            You can contact me on LinkedIn, through Email or by Phone.
+          </p>
+        </div>
+        <hr />
+        <div className="skills">
+          <h2>Contact Info</h2>
+          <div className="skill-row">
+            <img className="code" src={contactus} alt="phone" />
+            <h3>Raven Ontiveros</h3>
+            <ul>
+              <li>
+                <strong>Phone Number:</strong> (915)328-9112
+              </li>
+              <li>
+                <strong>Email:</strong> Raveblue21@yahoo.com
+              </li>
+              <li>
+                <strong>Gmail:</strong>ravenontiveros21@gmail.com
+              </li>
+            </ul>
+            <div className="skill-row">
+              <img className="more" src={application} alt="app" />
+              <h3>Links</h3>
+              <ul>
+                <li>
+                  <a
+                    className="resume"
+                    href="https://docs.google.com/document/d/1ErfEPhA3GOZN9oltJF4SZqJKsY5jPKuqQ6ooMSOPqgI/edit?usp=drive_link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <strong>Resume &rarr;</strong>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
